d43822edee1ef2aa1546cf285532e44da04fc3f7
Owner <owner@Owners-MacBook-Pro.local>
2025-06-10 20:40:15 -0700

    create models

--- littlelemon_project/restaurant/views.py
+++ littlelemon_project/restaurant/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from .models import Booking
+from .serializers import MenuItemSerializer
+from .serializers import BookingSerializer
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html', {})
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(rest_framework.viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
