fc49bcf7f2eb5e53e273d169dc1c797c928dfab8
Owner <owner@Owners-MacBook-Pro.local>
2025-08-13 12:25:01 -0700

    Add Readme.txt

--- littlelemon_project/restaurant/urls.py
+++ littlelemon_project/restaurant/urls.py
@@ -6,7 +6,7 @@ from .views import MenuItemView, SingleMenuItemView, MenuItemDetailView
 
 urlpatterns = [
     path('menu-items/', views.MenuItemView.as_view()),
-    path('menu-items<int:pk>/', views.SingleMenuItemView.as_view()),
+    path('menu-items/<int:pk>/', views.MenuItemDetailView.as_view()),
     path('message/', views.msg),
     # path('api/menu-items/<int:pk>/', views.MenuItemDetailView.as_view()),
 
